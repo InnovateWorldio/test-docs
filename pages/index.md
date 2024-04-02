9bae4ee6cfe678c93f53d0b8970df109fcc2e2da
Earl Wagner <earl.wagner@innovateworld.io>
2024-04-02 16:55:54 +0100

    initial commit

---
+++ pages/index.md
@@ -0,0 +1,3 @@
+# Goalflow Overview
+ 
+
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>GoalFlow</span>,
+  project: {
+    link: 'https://github.com/InnovateWorldio/goalflow',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/InnovateWorldio/goalflowdocs',
+  footer: {
+    text: 'Docs Repo',
+  },
+}
+
+export default config
